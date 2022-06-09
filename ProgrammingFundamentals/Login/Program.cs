6396fde88d04c93062d3aeaa88911e723ffe12a3
Emanuel Chavepeyer <daru12378@gmail.com>
2022-06-09 12:15:17 +0300

    more problem solving.

---
+++ ProgrammingFundamentals/Login/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Login
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            char[] key = text.ToCharArray();
+
+            Array.Reverse(key);
+
+            string password = new string(key);
+
+            for (int i = 1; i <= 4; i++)
+            {
+                string pwInput = Console.ReadLine();
+
+                if(pwInput != password)
+                {
+                    if(i < 4)
+                    {
+                        Console.WriteLine($"Incorrect password. Try again.");
+                        continue;
+                    }
+
+                    Console.WriteLine($"User {text} blocked!");
+                    break;
+                }
+
+                Console.WriteLine($"User {text} logged in.");
+                break;
+            }
+        }
+    }
+}
---
+++ ProgrammingFundamentals/PrintAndSum/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PrintAndSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var start = int.Parse(Console.ReadLine());
+            var end = int.Parse(Console.ReadLine());
+            var sum = 0;
+
+            for (int i = start; i <= end; i++)
+            {
+                Console.Write($"{i} ");
+
+                sum += i;
+            }
+
+            Console.WriteLine($"\nSum: {sum}");
+        }
+    }
+}
