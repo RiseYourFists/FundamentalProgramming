3ba9d329b6bfc2453b1ac52fe4cc6751c3106745
Emanuel Chavepeyer <daru12378@gmail.com>
2022-06-09 19:21:01 +0300

    Problem solving

---
+++ ProgrammingFundamentals/PadwanEquipment/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace PadwanEquipment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var budget = double.Parse(Console.ReadLine());
+
+            var countOfStudents = int.Parse(Console.ReadLine());
+
+            var lightsaberPrice = double.Parse(Console.ReadLine());
+
+            var robesPrice = double.Parse(Console.ReadLine());
+
+            var beltsPrice = double.Parse(Console.ReadLine());
+
+            var beltCount = countOfStudents - (countOfStudents / 6);
+
+            double amountOfLightsabers = Math.Ceiling(countOfStudents + (countOfStudents * 0.1));
+
+            double sum = (countOfStudents * robesPrice) + (beltCount * beltsPrice) + (amountOfLightsabers * lightsaberPrice);
+
+            if(sum <= budget)
+            {
+                Console.WriteLine($"The money is enough - it would cost {Math.Round(sum,2):f2}lv.");
+            }
+            else
+            {
+                double neededMoney = sum - budget;
+                Console.WriteLine($" John will need {Math.Round(neededMoney,2):f2}lv more.");
+            }
+        }
+    }
+}
---
+++ ProgrammingFundamentals/TriangleOfNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TriangleOfNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            for (int rows = 1; rows <= n; rows++)
+            {
+                for (int cols = 1; cols <= rows; cols++)
+                {
+                    Console.Write(rows);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
