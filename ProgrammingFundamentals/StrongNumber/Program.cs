ac90c038af6771736977254bf8704e846b4f9e6f
Emanuel Chavepeyer <daru12378@gmail.com>
2022-06-09 15:22:36 +0300

    Problem solving

---
+++ ProgrammingFundamentals/StrongNumber/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace StrongNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string factorialNumber = Console.ReadLine();
+
+            int result = 0;
+
+            for (int i = 0; i < factorialNumber.Length; i++)
+            {
+                int number = (int)Char.GetNumericValue(factorialNumber[i]);
+                
+                result += Factorial(number);
+            }
+
+            string comparisson = result.ToString();
+
+            if(comparisson == factorialNumber)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+
+        static int Factorial(int num)
+        {
+            int result = num;
+
+            for (int i = num - 1; i >= 1; i--)
+            {
+                result *= i;
+            }
+
+            if(result == 0)
+            {
+                result = 1;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ ProgrammingFundamentals/VendingMachine/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VendingMachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, double> vMachine = new Dictionary<string, double>()
+            {
+                {"Nuts", 2.0 },
+                {"Water", 0.7 },
+                {"Crisps", 1.5 },
+                {"Soda", 0.8 },
+                {"Coke", 1.0 }
+            };
+
+            string command = Console.ReadLine();
+            double balance = 0;
+            while(command != "Start")
+            {
+                double coin = double.Parse(command);
+
+                switch(coin)
+                {
+                    case 0.1:
+                    case 0.2:
+                    case 0.5:
+                    case 1:
+                    case 2:
+                        balance += coin;
+                        break;
+                    default:
+                        Console.WriteLine($"Cannot accept {coin}");
+                        break;
+
+                }
+
+                command = Console.ReadLine();
+            }
+
+            command = Console.ReadLine();
+
+            while(command != "End")
+            {
+                if(!vMachine.ContainsKey(command))
+                {
+                    Console.WriteLine("Invalid product");
+                    command = Console.ReadLine();
+                    continue;
+                }
+
+                vMachine.TryGetValue(command, out double price);
+
+                if (price > balance)
+                {
+                    Console.WriteLine("Sorry, not enough money");
+                }
+                else
+                {
+                    Console.WriteLine($"Purchased {command.ToLower()}");
+                    balance -= price;
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Change: {balance:f2}");
+        }
+    }
+}
