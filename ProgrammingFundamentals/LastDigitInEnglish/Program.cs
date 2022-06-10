715151eae4edb3b1ecf999897c9cb96cec60bc5b
Emanuel Chavepeyer <daru12378@gmail.com>
2022-06-10 22:04:32 +0300

    Problem solving

---
+++ ProgrammingFundamentals/LastDigitInEnglish/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace LastDigitInEnglish
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = Console.ReadLine();
+
+            int lastDigit = number.Length - 1;
+
+            var digit = number[lastDigit];
+            var text = string.Empty;
+
+            switch(digit)
+            {
+                case '0':
+                    text = "zero";
+                    break;
+                case '1':
+                    text = "one";
+                    break;
+                case '2':
+                    text = "two";
+                    break;
+                case '3':
+                    text = "three";
+                    break;
+                case '4':
+                    text = "four";
+                    break;
+                case '5':
+                    text = "five";
+                    break;
+                case '6':
+                    text = "six";
+                    break;
+                case '7':
+                    text = "seven";
+                    break;
+                case '8':
+                    text = "eight";
+                    break;
+                case '9':
+                    text = "nine";
+                    break;
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
---
+++ ProgrammingFundamentals/SortNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SortNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sortedNumbers = new List<int>();
+
+            for (int i = 0; i < 3; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                sortedNumbers.Add(number);
+            }
+
+            sortedNumbers.Sort();
+            sortedNumbers.Reverse();
+
+            foreach (var item in sortedNumbers)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
