983dffc8d483ec31dd2c6c9d7191eff6cf77826f
Emanuel Chavepeyer <daru12378@gmail.com>
2022-06-16 22:56:50 +0300

    Problem solving

---
+++ ProgrammingFundamentals/WaterOverflow/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace WaterOverflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int waterTankCapacity = 255;
+            int tankFull = 0;
+            var n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                var waterIntake = int.Parse(Console.ReadLine());
+
+                if(tankFull + waterIntake > waterTankCapacity)
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                    continue;
+                }
+
+                tankFull += waterIntake;
+            }
+
+            Console.WriteLine(tankFull);
+        }
+    }
+}
