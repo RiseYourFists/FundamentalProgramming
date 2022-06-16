b15fa3d1babe514f3c5c774789022edcea434870
Emanuel Chavepeyer <daru12378@gmail.com>
2022-06-16 17:21:32 +0300

    Problem solving

---
+++ ProgrammingFundamentals/PrintASCIITableInRange/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PrintASCIITableInRange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var start = int.Parse(Console.ReadLine());
+            var end = int.Parse(Console.ReadLine());
+
+            for (int i = start; i <= end; i++)
+            {
+                char currChar = (char)i;
+
+                Console.Write(currChar + " ");
+            }
+        }
+    }
+}
