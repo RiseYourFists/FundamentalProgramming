7d7b0bdd0925504199e98ba617f194f9540a765b
Emanuel Chavepeyer <daru12378@gmail.com>
2022-06-16 22:36:46 +0300

    Problem solving

---
+++ ProgrammingFundamentals/TriplesOfLatinLetters/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TriplesOfLatinLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            char startingPoint = 'a';
+            char endPoint = (char)(startingPoint + n - 1);
+
+            for (int firstRow = startingPoint; firstRow <= endPoint; firstRow++)
+            {
+                for (int secondRow = startingPoint; secondRow <= endPoint; secondRow++)
+                {
+                    for (int thirdRow = startingPoint; thirdRow <= endPoint; thirdRow++)
+                    {
+                        Console.WriteLine($"{(char)firstRow}{(char)secondRow}{(char)thirdRow}");
+                    }
+                }
+            }
+        }
+    }
+}
