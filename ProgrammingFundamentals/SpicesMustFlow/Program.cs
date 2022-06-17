757b7817c54a6afa245b854a0f82bb97fa5149e2
Emanuel Chavepeyer <daru12378@gmail.com>
2022-06-17 21:28:07 +0300

    Problem solving

---
+++ ProgrammingFundamentals/SpicesMustFlow/Program.cs
@@ -0,0 +1,29 @@
+﻿ using System;
+
+namespace SpicesMustFlow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var initialYield = long.Parse(Console.ReadLine());
+
+            long profit = 0;
+            var dayCounter = 0;
+
+            while(initialYield >= 100)
+            {
+                dayCounter++;
+                profit += initialYield - 26;
+                initialYield -= 10;
+            }
+
+            if(profit > 0)
+            {
+                Console.WriteLine($"{dayCounter}\n{profit - 26}");
+                return;
+            }
+                Console.WriteLine($"{dayCounter}\n{profit}");
+        }
+    }
+}
