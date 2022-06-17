0e94298c2b8c3eb2555f4b49ecba027d58407b48
Emanuel Chavepeyer <daru12378@gmail.com>
2022-06-17 16:21:31 +0300

    Problem solving

---
+++ ProgrammingFundamentals/BeerKegs/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Numerics;
+
+namespace BeerKegs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var kegsAmount = int.Parse(Console.ReadLine());
+            BigInteger maxValue = 0;
+            var maxName = string.Empty;
+            for (int i = 0; i < kegsAmount; i++)
+            {
+                string model = Console.ReadLine();
+                var radius = decimal.Parse(Console.ReadLine());
+                var height = BigInteger.Parse(Console.ReadLine());
+
+                decimal PI =(decimal)( Math.PI);
+                BigInteger result = (BigInteger)(PI * (radius * radius) * (decimal)height);
+
+                if(result > maxValue)
+                {
+                    maxValue = result;
+                    maxName = model;
+                }
+            }
+
+            Console.WriteLine(maxName);
+        }
+    }
+}
