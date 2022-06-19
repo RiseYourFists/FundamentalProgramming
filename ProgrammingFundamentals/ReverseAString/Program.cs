f97633859212abfec6ef1dada5ad63a640ab3442
Emanuel Chavepeyer <daru12378@gmail.com>
2022-06-19 21:57:18 +0300

    Problem solving

---
+++ ProgrammingFundamentals/ReverseAString/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ReverseAString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            Array.Reverse(text);
+            Console.WriteLine($"{string.Join(' ',text)}");
+        }
+    }
+}
---
+++ ProgrammingFundamentals/SumOfEvenNums/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace SumOfEvenNums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] evenNums = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).Select( x => { if (x % 2 == 0) return x; return 0; }).ToArray();
+
+            int sum = 0;
+
+            for (int i = 0; i < evenNums.Length; i++)
+            {
+                sum += evenNums[i];
+            }
+
+            Console.WriteLine(sum);
+
+
+        }
+    }
+}
