ef79636d399152855edcd21bd362263d23233a48
Emanuel Chavepeyer <daru12378@gmail.com>
2022-06-19 15:14:19 +0300

    Problem solving

---
+++ ProgrammingFundamentals/Pokemon/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Pokemon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            var pokePower = long.Parse(Console.ReadLine());// N
+            var initialPokePower = pokePower;
+            var distance = long.Parse(Console.ReadLine()); // M
+            var exhaustion = int.Parse(Console.ReadLine());// Y
+            int pokes = 0;
+
+            while(pokePower >= distance)
+            {
+                if(pokePower == ((double)initialPokePower / 2) && exhaustion != 0)
+                {
+                    pokePower /= exhaustion;
+                }
+
+                if(pokePower < distance)
+                {
+                    break;
+                }
+
+                pokePower -= distance;
+                pokes++;
+            }
+
+            Console.WriteLine($"{pokePower}\n{pokes}");
+
+        }
+    }
+}
