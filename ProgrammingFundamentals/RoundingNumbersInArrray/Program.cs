cc96048d8095c21748c4223251d2b1378c578a5c
Emanuel Chavepeyer <daru12378@gmail.com>
2022-06-19 20:45:07 +0300

    small changes

--- ProgrammingFundamentals/RoundingNumbersInArrray/Program.cs
+++ ProgrammingFundamentals/RoundingNumbersInArrray/Program.cs
@@ -8,7 +8,16 @@ namespace RoundingNumbersInArrray
         static void Main(string[] args)
         {
             double[] numbersToRound = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(double.Parse).ToArray();
-            double[] roundedNums = numbersToRound.Select(x => Math.Round(x, MidpointRounding.AwayFromZero)).Select(x => { if (x == -0) { x = 0;} return x; }).ToArray();
+            double[] roundedNums = numbersToRound.Select(x => Math.Round(x, MidpointRounding.AwayFromZero))
+                .Select( x => 
+                { 
+                    if (x == -0) 
+                    { 
+                        x = 0;
+                    } 
+                    return x; 
+                })
+                .ToArray();
 
             for (int i = 0; i < roundedNums.Length; i++)
             {
