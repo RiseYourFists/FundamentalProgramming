3c5d12cac98ff8842e90dc3f8ed625a3675af864
Emanuel Chavepeyer <daru12378@gmail.com>
2022-06-19 17:20:03 +0300

    Problem solving

---
+++ ProgrammingFundamentals/SnowBalls/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Numerics;
+
+namespace SnowBalls
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputs = int.Parse(Console.ReadLine());
+
+            var maxSnowBallSnow = 0;
+            var maxSnowBallTime = 0;
+            var maxSnowBallQuality = 0;
+            BigInteger maxValue = 0;
+
+            for (int i = 0; i < inputs; i++)
+            {
+                var snowBallSnow = int.Parse(Console.ReadLine());
+                var snowBallTime = int.Parse(Console.ReadLine());
+                var snowBallQuality = int.Parse(Console.ReadLine());
+
+                BigInteger snowBalls = snowBallSnow / snowBallTime;
+                BigInteger finalProduct = BigInteger.Pow(snowBalls, snowBallQuality);
+
+                if(finalProduct >= maxValue)
+                {
+                    maxValue = finalProduct;
+                    maxSnowBallTime = snowBallTime;
+                    maxSnowBallSnow = snowBallSnow;
+                    maxSnowBallQuality = snowBallQuality;
+                }
+
+            }
+
+            Console.WriteLine($"{maxSnowBallSnow} : {maxSnowBallTime} = {maxValue} ({maxSnowBallQuality})");
+        }
+    }
+}
