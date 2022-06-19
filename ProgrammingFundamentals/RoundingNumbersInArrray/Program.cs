510bd5b5bcd5a7f6fa9b27a6d9845925b73407db
Emanuel Chavepeyer <daru12378@gmail.com>
2022-06-19 20:31:56 +0300

    Problem solving

---
+++ ProgrammingFundamentals/RoundingNumbersInArrray/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace RoundingNumbersInArrray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbersToRound = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(double.Parse).ToArray();
+            double[] roundedNums = numbersToRound.Select(x => Math.Round(x, MidpointRounding.AwayFromZero)).Select(x => { if (x == -0) { x = 0;} return x; }).ToArray();
+
+            for (int i = 0; i < roundedNums.Length; i++)
+            {
+                Console.WriteLine($"{numbersToRound[i]} => {roundedNums[i]}");
+            }
+        }
+    }
+}
