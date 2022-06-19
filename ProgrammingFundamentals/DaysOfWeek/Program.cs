d70398ff792a281a3e93da268c147ad7f845f4ba
Emanuel Chavepeyer <daru12378@gmail.com>
2022-06-19 19:40:04 +0300

    Problem solving

---
+++ ProgrammingFundamentals/DaysOfWeek/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DaysOfWeek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dayNum = int.Parse(Console.ReadLine());
+
+            var week = new string[]
+            {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            };
+
+            if(dayNum >= 1 && dayNum <= week.Length )
+            {
+                Console.WriteLine($"{week[dayNum - 1]}");
+                return;
+            }
+
+            Console.WriteLine("Invalid day!");
+        }
+    }
+}
---
+++ ProgrammingFundamentals/PrintNumsInReverse/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace PrintNumsInReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputs = int.Parse(Console.ReadLine());
+
+            int[] numbers = new int[inputs];
+
+            for (int i = 0; i < inputs; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            Array.Reverse(numbers);
+
+            Console.WriteLine($"{string.Join(' ',numbers)}");
+        }
+    }
+}
