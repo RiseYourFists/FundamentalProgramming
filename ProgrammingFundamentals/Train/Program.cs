7b916775c68eb2e2f9efdf3100fb38a8dc50d125
Emanuel Chavepeyer <daru12378@gmail.com>
2022-06-20 20:02:41 +0300

    Problem solving

---
+++ ProgrammingFundamentals/Train/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var wagons = int.Parse(Console.ReadLine());
+
+            var sum = 0;
+            var passingers = new int[wagons];
+
+            for (int i = 0; i < wagons; i++)
+            {
+                var passingersPerWagon = int.Parse(Console.ReadLine());
+
+                passingers[i] = passingersPerWagon;
+                sum += passingersPerWagon;
+            }
+
+            foreach (var item in passingers)
+            {
+                Console.Write($"{item} ");
+            }
+
+            Console.WriteLine($"\n{sum}");
+        }
+    }
+}
