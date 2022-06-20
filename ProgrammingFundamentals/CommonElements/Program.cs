201b7ad47b313f49f68766505b61e57cc273f0f5
Emanuel Chavepeyer <daru12378@gmail.com>
2022-06-20 20:13:52 +0300

    Problem solving

---
+++ ProgrammingFundamentals/CommonElements/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CommonElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var checkingInput = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            var toCheck = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < toCheck.Length; i++)
+            {
+                var element = toCheck[i];
+
+                for (int j = 0; j < checkingInput.Length; j++)
+                {
+                    if(checkingInput[j] == element)
+                    {
+                        Console.Write($"{element} ");
+                    }
+                }
+            }
+        }
+    }
+}
