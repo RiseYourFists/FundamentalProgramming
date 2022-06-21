cff3f2527e34372fe2da852dd6cc425b6b9d076e
Emanuel Chavepeyer <daru12378@gmail.com>
2022-06-21 19:44:39 +0300

    Problem solving

---
+++ ProgrammingFundamentals/MagicSum/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace MagicSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            var magicNumber = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                var currNum = input[i];
+
+                for (int j = i + 1; j < input.Length; j++)
+                {
+                    if(currNum + input[j] == magicNumber)
+                    {
+                        Console.WriteLine($"{currNum} {input[j]}");
+                    }
+                }
+            }
+        }
+    }
+}
