4b35723e65fcdb7d02aa61b836f5024befc86f45
Emanuel Chavepeyer <daru12378@gmail.com>
2022-06-21 14:05:23 +0300

    Problem solving

---
+++ ProgrammingFundamentals/TopIntengers/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace TopIntengers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            var result = string.Empty;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                var currNum = numbers[i];
+                bool isGreater = true;
+
+                if(i == numbers.Length - 1)
+                {
+                    result += currNum;
+                    break;
+                }
+
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if(!(currNum > numbers[j]))
+                    {
+                        isGreater = false;
+                        break;
+                    }
+                }
+
+                if(isGreater)
+                {
+                    result += currNum + " ";
+                }
+
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
