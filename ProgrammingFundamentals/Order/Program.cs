dd3c81d9d930fb05c4488aa5d1217d91c4fc8fca
Emanuel Chavepeyer <daru12378@gmail.com>
2022-06-25 18:02:30 +0300

    problem solving

---
+++ ProgrammingFundamentals/Order/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Order
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var product = Console.ReadLine();
+            var quantity = int.Parse(Console.ReadLine());
+            var total = TotalPrice(product, quantity);
+            Console.WriteLine($"{Math.Round(total, 2):f2}");
+        }
+
+        static double TotalPrice(string product, int amount)
+        {
+            double result;
+
+            var price = 0d;
+
+            switch(product)
+            {
+                case "coffee":
+                    price = 1.5;
+                    break;
+                case "water":
+                    price = 1;
+                    break;
+                case "coke":
+                    price = 1.4;
+                    break;
+                case "snacks":
+                    price = 2;
+                    break;
+            }
+
+            result = price * amount;
+
+            return result;
+        }
+    }
+}
