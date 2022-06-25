f550207962836b5a6f8e6fc66fbf786ab6e259a0
Emanuel Chavepeyer <daru12378@gmail.com>
2022-06-25 18:07:43 +0300

    Problem solving

---
+++ ProgrammingFundamentals/RectangleArea/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RectangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var width = double.Parse(Console.ReadLine());
+            var height = double.Parse(Console.ReadLine());
+
+            var result = RectangleArea(width, height);
+
+            Console.WriteLine(result);
+        }
+
+        static double RectangleArea(double width, double height)
+        {
+            var result = width * height;
+            return result;
+        }
+    }
+}
