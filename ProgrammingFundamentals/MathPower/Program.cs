c5f0e8070b17a6f054e8e2b0384b89a24135e8b0
Emanuel Chavepeyer <daru12378@gmail.com>
2022-06-25 19:31:29 +0300

    Problem solving

---
+++ ProgrammingFundamentals/MathPower/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MathPower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = double.Parse(Console.ReadLine());
+            int pow = int.Parse(Console.ReadLine());
+
+            var result = Pow(number, pow);
+
+            Console.WriteLine(result);
+        }
+
+        private static double Pow(double number, int pow)
+        {
+            var result = number;
+            
+            for (int i = 1; i <= pow - 1; i++)
+            {
+                result *= number;
+            }
+
+            return result;
+        }
+    }
+}
