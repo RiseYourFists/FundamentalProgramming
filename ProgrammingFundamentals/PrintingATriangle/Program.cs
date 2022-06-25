fc5fe1abd4529e1c5e29363757f50b03d4748b4d
Emanuel Chavepeyer <daru12378@gmail.com>
2022-06-25 17:28:47 +0300

    Problem solving

---
+++ ProgrammingFundamentals/PrintingATriangle/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PrintingATriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var size = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= size; i++)
+            {
+                PrintLine(i);
+            }
+            for (int i = size - 1; i > 0; i--)
+            {
+                PrintLine(i);
+            }
+        }
+
+        static void PrintLine(int len)
+        {
+            for (int i = 1; i <= len; i++)
+            {
+                Console.Write($"{i} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
