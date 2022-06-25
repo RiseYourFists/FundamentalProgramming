efc9beaf5c9cf0ff9fc2233858ecb1fdbfa4a6e7
Emanuel Chavepeyer <daru12378@gmail.com>
2022-06-25 14:15:27 +0300

    Problem solving

---
+++ ProgrammingFundamentals/SignOfIntNums/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SignOfIntNums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{SignOfInt(number)}");
+        }
+
+        static string SignOfInt(int number)
+        {
+            var result = string.Empty;
+
+            if(number > 0)
+            {
+                result = $"The number {number} is positive.";
+            }
+            else if(number < 0)
+            {
+                result = $"The number {number} is negative.";
+            }
+            else
+            {
+                result = $"The number {number} is zero.";
+            }
+
+            return result;
+        }
+    }
+}
