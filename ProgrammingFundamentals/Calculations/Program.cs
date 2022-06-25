d109f0ee905c9eced66d704f1b64329c838512d9
Emanuel Chavepeyer <daru12378@gmail.com>
2022-06-25 14:43:44 +0300

    Problem solving

---
+++ ProgrammingFundamentals/Calculations/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Calculations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var operation = Console.ReadLine();
+            var firstNum = double.Parse(Console.ReadLine());
+            var secondNum = double.Parse(Console.ReadLine());
+
+            switch (operation)
+            {
+                case "add":
+                    Add(firstNum, secondNum);
+                    break;
+                case "subtract":
+                    Subtract(firstNum, secondNum);
+                    break;
+                case "multiply":
+                    Multiply(firstNum, secondNum);
+                    break;
+                case "divide":
+                    Divide(firstNum, secondNum);
+                    break;
+            }
+        }
+
+        private static void Divide(double firstNum, double secondNum)
+        {
+            Console.WriteLine(firstNum / secondNum);
+        }
+
+        private static void Multiply(double firstNum, double secondNum)
+        {
+            Console.WriteLine(firstNum * secondNum);
+        }
+
+        private static void Subtract(double firstNum, double secondNum)
+        {
+            Console.WriteLine(firstNum - secondNum);
+        }
+
+        private static void Add(double firstNum, double secondNum)
+        {
+            Console.WriteLine(firstNum + secondNum);
+        }
+    }
+}
