f569885f883c5a9e4a38b903284d21f8f5dc0456
Emanuel Chavepeyer <daru12378@gmail.com>
2022-06-25 18:49:41 +0300

    Problem solving

---
+++ ProgrammingFundamentals/RepeatAString/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace RepeatAString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var @string = Console.ReadLine();
+            var timesToRepeat = int.Parse(Console.ReadLine());
+
+            var text = RepeatString(@string, timesToRepeat);
+            Console.WriteLine(text);
+        }
+
+        static string RepeatString(string line, int times)
+        {
+            StringBuilder text = new StringBuilder();
+            for (int i = 0; i < times; i++)
+            {
+                text.Append(line);
+            }
+            string result = text.ToString();
+
+            return result;
+        }
+    }
+}
