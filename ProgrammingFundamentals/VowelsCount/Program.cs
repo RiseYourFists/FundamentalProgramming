b3b5806d7025ddb0d9eb9b9cb0090be33a37d07d
Emanuel Chavepeyer <daru12378@gmail.com>
2022-06-28 17:59:06 +0300

    Problem solving

---
+++ ProgrammingFundamentals/VowelsCount/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace VowelsCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = Console.ReadLine();
+            int vowels = Vowels(text.ToLower());
+            Console.WriteLine(vowels);
+        }
+
+        static int Vowels(string text)
+        {
+            int vowels = 0;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char letter = text[i];
+
+                switch(letter)
+                {
+                    case 'a':
+                        vowels++;
+                        break;
+                    case 'o':
+                        vowels++;
+                        break;
+                    case 'u':
+                        vowels++;
+                        break;
+                    case 'e':
+                        vowels++;
+                        break;
+                    case 'i':
+                        vowels++;
+                        break;
+                }
+            }
+
+            return vowels;
+        }
+    }
+}
