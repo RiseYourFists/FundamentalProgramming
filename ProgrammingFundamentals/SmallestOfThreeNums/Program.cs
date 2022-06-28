d12702c6ad9f0eb1451a0e1bc4d3255d4852b372
Emanuel Chavepeyer <daru12378@gmail.com>
2022-06-28 17:41:06 +0300

    Problem solving

---
+++ ProgrammingFundamentals/SmallestOfThreeNums/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SmallestOfThreeNums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var threeNumbers = new int[3];
+
+            for (int i = 0; i < threeNumbers.Length; i++)
+            {
+                threeNumbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            var lowestNumber = Lowest(threeNumbers);
+
+            Console.WriteLine(lowestNumber);
+        }
+
+        static int Lowest(int[] numbers)
+        {
+            int lowest = numbers[0];
+
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                int currNum = numbers[i];
+                if(currNum < lowest)
+                {
+                    lowest = currNum;
+                }
+            }
+
+            return lowest;
+        }
+    }
+}
