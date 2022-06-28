9abb80c5c7b6c57565713a44d64ed275f229ad30
Emanuel Chavepeyer <daru12378@gmail.com>
2022-06-28 22:24:21 +0300

    Problem solving

---
+++ ProgrammingFundamentals/ASCIIMethodInRange/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ASCIIMethodInRange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var valueOne = char.Parse(Console.ReadLine());
+            var ValueTwo = char.Parse(Console.ReadLine());
+
+            int firstChar = valueOne;
+            int secondChar = ValueTwo;
+
+            if(firstChar < secondChar)
+            {
+                ASCIIinRange(valueOne, ValueTwo);
+            }
+            else
+            {
+                ASCIIinRange(ValueTwo, valueOne);
+            }
+        }
+
+        static void ASCIIinRange(char start, char end)
+        {
+            for (int i = start + 1; i < end; i++)
+            {
+                char currChar = (char)i;
+
+                Console.Write($"{currChar} ");
+            }
+        }
+    }
+}
