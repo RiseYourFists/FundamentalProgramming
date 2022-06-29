599e98b4ca70d1d3915c64b106d1d63f569e393e
Emanuel Chavepeyer <daru12378@gmail.com>
2022-06-29 15:27:04 +0300

    Problem solving

---
+++ ProgrammingFundamentals/MiddleChars/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MiddleChars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = Console.ReadLine();
+            var middleChars = MiddleChars(text);
+            Console.WriteLine(middleChars);
+        }
+
+        static string MiddleChars(string text)
+        {
+            var result = string.Empty;
+
+            if(text.Length % 2 == 0)
+            {
+                var len = (text.Length / 2) - 1;
+                result = string.Concat(text[len], text[len + 1]);
+            }
+            else
+            {
+                var len = text.Length / 2;
+                result = text[len].ToString();
+            }
+
+            return result;
+        }
+    }
+}
