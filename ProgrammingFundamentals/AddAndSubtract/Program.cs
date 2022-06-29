305f0e31305bf6e67a1cd3eb19e06e5d675920df
Emanuel Chavepeyer <daru12378@gmail.com>
2022-06-29 15:06:04 +0300

    Problem solving

---
+++ ProgrammingFundamentals/AddAndSubtract/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace AddAndSubtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new int[3];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            var firstOperation = Add(numbers[0], numbers[1]);
+            var lastOperation = Subtract(firstOperation, numbers[2]);
+            Console.WriteLine(lastOperation);
+
+        }
+
+        static int Add(int numberOne, int numberTwo)
+        {
+            return numberOne + numberTwo;
+        }
+
+        static int Subtract(int numberOne, int numberTwo)
+        {
+            return numberOne - numberTwo;
+        }
+    }
+}
