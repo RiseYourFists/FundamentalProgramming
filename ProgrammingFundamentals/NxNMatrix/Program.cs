d8098a7fd23574add258f10b62a2f6be8bba2ef4
Emanuel Chavepeyer <daru12378@gmail.com>
2022-06-29 15:45:02 +0300

    Problem solving

---
+++ ProgrammingFundamentals/NxNMatrix/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace NxNMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                PrintLine(n);
+            }
+        }
+
+        static void PrintLine(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"{n} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
