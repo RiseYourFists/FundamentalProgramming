4f0a019f371924f4b590a2608bc4f1fbf083cb80
Emanuel Chavepeyer <daru12378@gmail.com>
2022-06-29 23:21:21 +0300

    Problem solving

---
+++ ProgrammingFundamentals/PalindromeOfIntengers/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace PalindromeOfIntengers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var command = Console.ReadLine();
+
+            while(command != "END")
+            {
+                var number = int.Parse(command);
+                var result = (IsNumPalindrome(number))? "true": "false";
+                Console.WriteLine(result);
+
+                command = Console.ReadLine();
+            }
+        }
+
+        static bool IsNumPalindrome(int num)
+        {
+            string numToText = num.ToString();
+            var digits = new char[numToText.Length];
+
+            for (int i = 0; i < numToText.Length; i++)
+            {
+                digits[i] = numToText[i];
+            }
+
+            Array.Reverse(digits);
+            var resultText = string.Concat(digits);
+            var result = int.Parse(resultText);
+
+            if(result == num)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
