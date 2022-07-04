0cddb82b3a4d8eb87a4fed8aa584c2b7727fad18
Emanuel Chavepeyer <daru12378@gmail.com>
2022-07-04 21:02:58 +0300

    Problem solving

---
+++ ProgrammingFundamentals/GaussTrick/Program.cs
@@ -0,0 +1,42 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GaussTrick
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            int lenght = ValidateLenght(numbers.Count);
+            var result = new List<int>();
+
+            for (int i = 0; i < lenght; i++)
+            {
+                result.Add(numbers[i] + numbers[(numbers.Count - 1) - i]);
+            }
+
+            if(numbers.Count % 2 == 1)
+            {
+                result.Add(numbers[lenght]);
+            }
+
+            Console.WriteLine($"{string.Join(' ', result)}");
+        }
+
+        private static int ValidateLenght(int lenght)
+        {
+            if (lenght % 2 == 0)
+            {
+                lenght /= 2;
+            }
+            else
+            {
+                lenght = (lenght - 1) / 2;
+            }
+
+            return lenght;
+        }
+    }
+}
