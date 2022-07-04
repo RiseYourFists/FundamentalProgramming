958ddc1d31a12aa341a9d2600b0613bcb59e59e6
Emanuel Chavepeyer <daru12378@gmail.com>
2022-07-04 22:48:14 +0300

    Problem solving

---
+++ ProgrammingFundamentals/ListOfProducts/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListOfProducts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var products = new List<string>();
+
+            int amount = int.Parse(Console.ReadLine());
+            for (int i = 0; i < amount; i++)
+            {
+                string product = Console.ReadLine();
+                products.Add(product);
+            }
+
+            products.Sort();
+
+            for (int i = 0; i < products.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}.{products[i]}");
+            }
+        }
+    }
+}
