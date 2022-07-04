bd87896bc997d4a45515eafbf90a1fea525099bf
Emanuel Chavepeyer <daru12378@gmail.com>
2022-07-04 23:26:00 +0300

    Problem solving

---
+++ ProgrammingFundamentals/RemoveNegativeAndReverse/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace RemoveNegativeAndReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            numbers.RemoveAll(x => x < 0);
+            numbers.Reverse();
+            if(numbers.Count == 0)
+            {
+                Console.WriteLine("empty");
+                return;
+            }
+
+            Console.WriteLine($"{string.Join(' ', numbers)}");
+        }
+    }
+}
