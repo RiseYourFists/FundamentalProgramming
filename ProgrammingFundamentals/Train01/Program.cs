3e895af9f491ee8c6f4cc889d5108ee7d37d2891
Emanuel Chavepeyer <daru12378@gmail.com>
2022-07-05 18:19:55 +0300

    Problem solving

---
+++ ProgrammingFundamentals/Train01/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace Train01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var wagons = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            var capacity = int.Parse(Console.ReadLine());
+            var command = Console.ReadLine();
+
+            while(command != "end")
+            {
+                var input = command.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                if(input[0] == "Add")
+                {
+                    wagons.Add(int.Parse(input[1]));
+                    command = Console.ReadLine();
+                    continue;
+                }
+
+                int passingers = int.Parse(input[0]);
+                for (int i = 0; i < wagons.Count; i++)
+                {
+                    if(passingers <= capacity - wagons[i])
+                    {
+                        wagons[i] += passingers;
+                        break;
+                    }
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(' ', wagons));
+        }
+    }
+}
