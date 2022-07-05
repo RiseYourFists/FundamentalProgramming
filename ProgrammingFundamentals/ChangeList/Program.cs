f8595fa5c1d3a9d4eb84f6a75eba63c6f395d1aa
Emanuel Chavepeyer <daru12378@gmail.com>
2022-07-05 19:01:24 +0300

    Problem solving

---
+++ ProgrammingFundamentals/ChangeList/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ChangeList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+            var command = Console.ReadLine();
+
+            while (command != "end")
+            {
+                var input = command.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                var action = input[0];
+
+                switch (action)
+                {
+                    case "Delete":
+                        number.RemoveAll(x => x == int.Parse(input[1]));
+                        break;
+                    case "Insert":
+                        number.Insert(int.Parse(input[2]), int.Parse(input[1]));
+                        break;
+                }
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(' ', number));
+        }
+    }
+}
