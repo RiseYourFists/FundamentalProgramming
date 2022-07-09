20969facde74d92558b036c1ee948e29d2bf1c66
Emanuel Chavepeyer <daru12378@gmail.com>
2022-07-09 20:15:33 +0300

    Problem solving

---
+++ ProgrammingFundamentals/AppendArrays/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AppendArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arrays = Console.ReadLine().Split('|').ToList();
+            var result = new List<int>();
+
+            for (int i = arrays.Count - 1; i >= 0; i--)
+            {
+                result.AddRange(arrays[i].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList());  
+            }
+
+            Console.WriteLine(string.Join(' ', result));
+        }
+    }
+}
