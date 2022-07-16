7425afc5511da4fc6c4f0db8fb087a877197d69d
Emanuel Chavepeyer <daru12378@gmail.com>
2022-07-16 20:35:32 +0300

    Problem solving

---
+++ ProgrammingFundamentals/BigFactorial/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Numerics;
+namespace BigFactorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var times = int.Parse(Console.ReadLine());
+
+            BigInteger result = 1;
+
+            for (int i = times; i > 0; i--)
+            {
+                result *= i;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
