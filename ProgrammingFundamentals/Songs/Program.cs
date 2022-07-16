3b52772d93d3202c3b3c29acb7af353e5baf7783
Emanuel Chavepeyer <daru12378@gmail.com>
2022-07-16 21:01:34 +0300

    Problem solving

---
+++ ProgrammingFundamentals/Songs/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Songs
+{
+    class Songs
+    {
+        public Songs(string name, string type, string time)
+        {
+            Name = name;
+            Type = type;
+            Time = time;
+        }
+
+        public string Name{ get; set; }
+        public string Type { get; set; }
+        public string Time{ get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var songList = new List<Songs>();
+
+            var n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine().Split('_',StringSplitOptions.RemoveEmptyEntries).ToList();
+                string type = input[0];
+                string name = input[1];
+                string time = input[2];
+
+                var newSong = new Songs(name, type, time);
+                songList.Add(newSong);
+            }
+
+            var command = Console.ReadLine();
+
+            if(command == "all")
+            {
+                foreach (var item in songList)
+                {
+                    Console.WriteLine(item.Name);
+                }
+            }
+            else
+            {
+                var result = songList.FindAll(x => x.Type == command).ToList();
+                foreach (var item in result)
+                {
+                    Console.WriteLine(item.Name);
+                }
+            }
+        }
+    }
+}
