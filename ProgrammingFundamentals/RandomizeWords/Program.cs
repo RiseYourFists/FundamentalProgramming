1d9e5fcf283f6e7ca3cd155f2b6f186122cef103
Emanuel Chavepeyer <daru12378@gmail.com>
2022-07-16 19:54:30 +0300

    Problem solving

---
+++ ProgrammingFundamentals/RandomizeWords/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace RandomizeWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            Random randomNum = new Random();
+            for (int i = 0; i < input.Length; i++)
+            {
+                int index = randomNum.Next(0, input.Length - 1);
+                string tempString = input[i];
+                input[i] = input[index];
+                input[index] = tempString;
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine , input));
+        }
+    }
+}
