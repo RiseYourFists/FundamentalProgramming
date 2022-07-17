9dfbd1705124ceb226b90fb88dd03695a3cf2882
Emanuel Chavepeyer <daru12378@gmail.com>
2022-07-17 22:20:29 +0300

    Problem solving

---
+++ ProgrammingFundamentals/Students/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Students
+{
+    class Student
+    {
+        public Student(string firstName, string lastName, int age, string location)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            HomeTown = location;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string HomeTown { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            var Studends = new List<Student>();
+
+            while(input != "end")
+            {
+                var tokens = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                var firstName = tokens[0];
+                var lastName = tokens[1];
+                var age = int.Parse(tokens[2]);
+                var location = tokens[3];
+
+                var newStudent = new Student(firstName, lastName, age, location);
+                Studends.Add(newStudent);
+
+                input = Console.ReadLine();
+            }
+
+            string filter = Console.ReadLine();
+
+            var result = Studends.Where(x => x.HomeTown == filter).ToList();
+
+            foreach (var item in result)
+            {
+                Console.WriteLine($"{item.FirstName} {item.LastName} is {item.Age} years old.");
+            }
+        }
+    }
+}
