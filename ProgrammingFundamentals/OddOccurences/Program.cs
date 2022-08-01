00dc04df8ca454488bbf25016bcf5a76e3007888
Emanuel Chavepeyer <daru12378@gmail.com>
2022-08-01 17:40:06 +0300

    Problem solving

---
+++ ProgrammingFundamentals/OddOccurences/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OddOccurences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(x => x.ToLower()).ToArray();
+
+            var OccuranceList = new Dictionary<string, int>();
+
+            foreach (var item in input)
+            {
+                if(!OccuranceList.ContainsKey(item))
+                {
+                    OccuranceList.Add(item, 1);
+                    continue;
+                }
+
+                OccuranceList[item]++;
+            }
+
+            var result = OccuranceList.Where(x => x.Value % 2 == 1);
+
+            foreach (var item in result)
+            {
+                Console.Write($"{item.Key} ");
+            }
+        }
+    }
+}
