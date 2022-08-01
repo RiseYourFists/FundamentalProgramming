c55e471d01ad32820b3b0a5f629e728053ca67df
Emanuel Chavepeyer <daru12378@gmail.com>
2022-08-01 17:14:09 +0300

    Problem solving

---
+++ ProgrammingFundamentals/CountRealNumbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CountRealNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbersInput = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            var numbersDictionary = new SortedDictionary<int, int>();
+
+            foreach (var item in numbersInput)
+            {
+                if(!numbersDictionary.ContainsKey(item))
+                {
+                    numbersDictionary.Add(item, 1);
+                    continue;
+                }
+
+                numbersDictionary[item]++;
+            }
+
+            foreach (var item in numbersDictionary)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
