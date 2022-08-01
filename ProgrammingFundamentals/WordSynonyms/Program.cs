02d7078ae02ebc842166a6ff05941a0f7970a5c4
Emanuel Chavepeyer <daru12378@gmail.com>
2022-08-01 18:11:01 +0300

    Problem solving

---
+++ ProgrammingFundamentals/WordSynonyms/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace WordSynonyms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var wordSynonyms = new Dictionary<string, List<string>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var word = Console.ReadLine();
+                var synonym = Console.ReadLine();
+
+                if(!wordSynonyms.ContainsKey(word))
+                {
+                    wordSynonyms.Add(word, new List<string> { synonym });
+                    continue;
+                }
+
+                wordSynonyms[word].Add(synonym);
+            }
+
+            foreach (var item in wordSynonyms)
+            {
+                Console.WriteLine($"{item.Key} - {string.Join(", ", item.Value)}");
+            }
+        }
+    }
+}
