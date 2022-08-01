726eb5a42dbdf614f85df0b35232bd99287dff3c
Emanuel Chavepeyer <daru12378@gmail.com>
2022-08-01 15:30:42 +0300

    Problem solving

---
+++ ProgrammingFundamentals/BinaryDigitsCount/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BinaryDigitsCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = int.Parse(Console.ReadLine());
+            var bitNumber = ConvertToBinary(number);
+            var compare = Console.Read();
+            var matches = 0;
+
+            for (int i = 0; i < bitNumber.Length; i++)
+            {
+                if(bitNumber[i] == compare)
+                {
+                    matches++;
+                }
+            }
+
+            Console.WriteLine(matches);
+
+        }
+
+        static string ConvertToBinary(int number)
+        {
+            var convertedNumber = new List<int>();
+
+            while(number > 0)
+            {
+                convertedNumber.Add(number % 2);
+                number /= 2;
+            }
+            convertedNumber.Reverse();
+
+            return string.Join("",convertedNumber);
+        }
+    }
+}
