4aae1b2b81bb3ff75dcaaf72c46f06177ae62025
Emanuel Chavepeyer <daru12378@gmail.com>
2022-08-01 19:26:49 +0300

    Problem solving

---
+++ ProgrammingFundamentals/Word Filter/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace WordFilter
+{
+    class Program
+    {
+        static void Main(string[]  args)
+        {
+            var input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Where(x => x.Length % 2 == 0).ToArray();
+            Console.WriteLine(string.Join(Environment.NewLine, input).TrimEnd());
+        }
+    }
+}
