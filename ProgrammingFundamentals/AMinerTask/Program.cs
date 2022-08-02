c6b13413de2aaf3d470e2958d88a570a791f00f4
Emanuel Chavepeyer <daru12378@gmail.com>
2022-08-02 22:08:20 +0300

    Problem solving

---
+++ ProgrammingFundamentals/AMinerTask/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AMinerTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inventory = new Dictionary<string, long>();
+
+            while (true)
+            {
+                var ore = Console.ReadLine();
+
+                if(ore == "stop")
+                {
+                    break;
+                }
+
+                var amount = long.Parse(Console.ReadLine());
+
+                if(!inventory.ContainsKey(ore))
+                {
+                    inventory.Add(ore, amount);
+                    continue;
+                }
+
+                inventory[ore] += amount;
+
+            }
+
+            foreach (var item in inventory)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
