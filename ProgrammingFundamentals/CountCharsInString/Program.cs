a7112ba8d001ce6aa9f6bbe35a3cbfb25fba4de8
Emanuel Chavepeyer <daru12378@gmail.com>
2022-08-02 21:51:37 +0300

    Problem solving

---
+++ ProgrammingFundamentals/CountCharsInString/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CountCharsInString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var occurances = new Dictionary<string, int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                var currChar = input[i].ToString();
+
+                if(currChar == " ")
+                {
+                    continue;
+                }
+
+                if(!occurances.ContainsKey(currChar))
+                {
+                    occurances.Add(currChar, 1);
+                    continue;
+                }
+
+                occurances[currChar]++;
+            }
+
+            foreach (var item in occurances)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
