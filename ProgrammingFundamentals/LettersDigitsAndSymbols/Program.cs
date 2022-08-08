b6710e4acc2dbc63f4e40d398f751ae587440f0c
Emanuel Chavepeyer <daru12378@gmail.com>
2022-08-08 22:33:55 +0300

    Problem solving

---
+++ ProgrammingFundamentals/LettersDigitsAndSymbols/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace LettersDigitsAndSymbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var numbers = string.Empty;
+            var letters = string.Empty;
+            var symbols = string.Empty;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char digit = input[i];
+
+                if(char.IsDigit(digit))
+                {
+                    numbers += digit;
+                    continue;
+                }
+
+                if(char.IsLetter(digit))
+                {
+                    letters += digit;
+                    continue;
+                }
+
+                symbols += digit;
+            }
+
+            Console.WriteLine($"{numbers}\n{letters}\n{symbols}");
+        }
+    }
+}
