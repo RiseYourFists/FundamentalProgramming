6ed0fd429a4244c4279a37f484f439420b787f39
Emanuel Chavepeyer <daru12378@gmail.com>
2022-08-08 21:16:43 +0300

    Problem solving

---
+++ ProgrammingFundamentals/ReverseText/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ReverseText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var input = Console.ReadLine();
+                if(input == "end")
+                {
+                    break;
+                }
+
+                var ch = input.ToCharArray();
+                Array.Reverse(ch);
+                var reversed = new string(ch);
+
+                Console.WriteLine($"{input} = {reversed}");
+                
+            }
+        }
+    }
+}
