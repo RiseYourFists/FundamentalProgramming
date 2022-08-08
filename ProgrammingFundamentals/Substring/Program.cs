4d5b128491499bc7576653b8c0d4c9637ec750b2
Emanuel Chavepeyer <daru12378@gmail.com>
2022-08-08 21:59:43 +0300

    Problem solving

---
+++ ProgrammingFundamentals/Substring/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var removeKey = Console.ReadLine();
+            var input = Console.ReadLine();
+
+            while (input.Contains(removeKey))
+            {
+                 input = input.Replace(removeKey, "");
+            }
+
+            Console.WriteLine(input);
+        }
+    }
+}
