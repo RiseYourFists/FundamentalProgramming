38941b48b91775fd1662b4f1e61299edd8347c00
Emanuel Chavepeyer <daru12378@gmail.com>
2022-08-08 21:32:40 +0300

    Problem solving

---
+++ ProgrammingFundamentals/RepeatingStrings/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace RepeatingStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            var sb = new StringBuilder();
+
+            foreach (var item in input)
+            {
+                for (int i = 0; i < item.Length; i++)
+                {
+                    sb.Append(item);
+                }
+            }
+
+            Console.WriteLine(sb);
+        }
+    }
+}
