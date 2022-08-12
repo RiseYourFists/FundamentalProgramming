937d7d9a4840375cc8118757d1ee8ffe4624b6b5
Emanuel Chavepeyer <daru12378@gmail.com>
2022-08-12 17:34:45 +0300

    problem solving

---
+++ ProgrammingFundamentals/ExtractFile/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ExtractFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            var extensionIndex = input.LastIndexOf('.');
+            var fileNameIndex = input.LastIndexOf('\\');
+            var extension = input.Substring(extensionIndex + 1);
+            var len = (input.Length - (fileNameIndex + 1)) - (extension.Length + 1);
+            var fileName = input.Substring(fileNameIndex + 1, len);
+            Console.WriteLine($"File name: {fileName}\nFile extension: {extension}");
+        }
+    }
+}
