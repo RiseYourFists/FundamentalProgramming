c7eff711cc5961d74a370558d8efdbe2409b6808
Emanuel Chavepeyer <daru12378@gmail.com>
2022-08-12 17:14:38 +0300

    Problem solving

---
+++ ProgrammingFundamentals/CaesarCipher/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace CaesarCipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            var result = Encrypt(input);
+            Console.WriteLine(result);
+        }
+
+        static string Encrypt(string line)
+        {
+            var result = new StringBuilder();
+
+            for (int i = 0; i < line.Length; i++)
+            {
+                var currCh = line[i];
+                var push = (char)(currCh + 3);
+                result.Append(push);
+            }
+            return result.ToString();
+        }
+    }
+}
