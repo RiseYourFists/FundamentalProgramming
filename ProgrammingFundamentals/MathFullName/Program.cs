90ef70040fb84f360125e6d4be458c6b7dfbb0aa
Emanuel Shavepeyer <daru12378@gmail.com>
2022-08-16 23:21:18 +0300

    Problem solving

---
+++ ProgrammingFundamentals/MathFullName/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace MatchFullName
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var namePattern = @"\b([A-Z][a-z]+) ([A-Z][a-z]+)\b";
+
+            Regex regex = new Regex(namePattern);
+            MatchCollection match = regex.Matches(input);
+
+            foreach (Match item in match)
+            {
+                Console.Write(item + " ");
+            }
+
+        }
+    }
+}
