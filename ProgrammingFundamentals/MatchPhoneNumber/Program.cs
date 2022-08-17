b62ef5ea296e6a19a7d14b73a9765e304b9981ca
Emanuel Shavepeyer <daru12378@gmail.com>
2022-08-17 12:44:10 +0300

    Problem solving

---
+++ ProgrammingFundamentals/MatchPhoneNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace MatchPhoneNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var validPhoneNumberPattern = @"(?:\+359)( |-)2\1(\d{3})\1(\d{4})\b";
+
+            var input = Console.ReadLine();
+
+            var regex = new Regex(validPhoneNumberPattern);
+            var match = regex.Matches(input);
+
+            var matchedPhones = match.Cast<Match>().Select(x => x.Value.Trim()).ToArray();
+
+            Console.WriteLine(string.Join(", ", matchedPhones));
+        }
+    }
+}
